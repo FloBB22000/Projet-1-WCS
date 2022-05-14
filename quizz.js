e4af5c0a21332d539352366d249c1db07cf32bba
FloBB22000 <florence.goslino@sncf.fr>
2022-05-14 14:07:42 +0200

    ajout images et textes

--- quizz.js
+++ quizz.js
@@ -3,16 +3,16 @@ function submitAnswers(){
 	var score = 0;
 
 	// Get user input
-	var q1 = document.forms["quizzForm"]["q1"].value;
-	var q2 = document.forms["quizzForm"]["q2"].value;
-	var q3 = document.forms["quizzForm"]["q3"].value;
-	var q4 = document.forms["quizzForm"]["q4"].value;
-	var q5 = document.forms["quizzForm"]["q5"].value;
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
 
 	// Validation
 	for(i = 1; i <= total; i++){
 		if(eval('q'+i) == null || eval('q'+i) == ''){
-			alert('You missed question' +i);
+			alert(' Vous avez oublié de répondre à une ou plusieurs questions');
 			return false;
 		}
 	}
@@ -29,8 +29,8 @@ function submitAnswers(){
 
 	// Display results
 	var results = document.getElementById('results');
-	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
-	alert('You scored '+score+' out of ' +total);
+	results.innerHTML = '<h3>Votre score <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+	alert(' Votre score '+score+' sur ' +total);
 
 	return false;
 }

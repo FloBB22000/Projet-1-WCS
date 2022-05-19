2b4875455d2536c81a1ab7b3fbcbf8cc315e7ea1
FloBB22000 <florence.goslino@sncf.fr>
2022-05-19 14:43:55 +0200

    modif css

--- quizz.js
+++ quizz.js
@@ -1,13 +1,13 @@
 function submitAnswers(){
-	var total = 5;
-	var score = 0;
+	let total = 5;
+	let score = 0;
 
 	// Get user input
-	var q1 = document.forms["quizForm"]["q1"].value;
-	var q2 = document.forms["quizForm"]["q2"].value;
-	var q3 = document.forms["quizForm"]["q3"].value;
-	var q4 = document.forms["quizForm"]["q4"].value;
-	var q5 = document.forms["quizForm"]["q5"].value;
+	let q1 = document.forms["quizForm"]["q1"].value;
+	let q2 = document.forms["quizForm"]["q2"].value;
+	let q3 = document.forms["quizForm"]["q3"].value;
+	let q4 = document.forms["quizForm"]["q4"].value;
+	let q5 = document.forms["quizForm"]["q5"].value;
 
 	// Validation
 	for(i = 1; i <= total; i++){
@@ -18,7 +18,7 @@ function submitAnswers(){
 	}
 
 	// Set correct answers
-	var answers = ["b","a","d","b","d"];
+	let answers = ["b","a","d","b","d"];
 
 	// Check answers
 	for(i = 1; i <= total; i++){	
@@ -28,7 +28,7 @@ function submitAnswers(){
 	}
 
 	// Display results
-	var results = document.getElementById('results');
+	let results = document.getElementById('results');
 	results.innerHTML = '<h3>Votre score <span>'+score+'</span> out of <span>'+total+'</span></h3>';
 	alert(' Votre score '+score+' sur ' +total);
 
